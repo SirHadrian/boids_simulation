39f3783de133665e4c89960a82e6273dcd6ec4e7
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-10-09 16:42:23 +0300

    basic setup

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+// Create scene and camera
+const scene: THREE.Scene = new THREE.Scene();
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000,
+);
+
+// Renderer
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+const target = document.getElementById('app');
+target?.appendChild(renderer.domElement);
+
+// Orbit controls / camera position
+new OrbitControls(camera, renderer.domElement);
+camera.position.z = 20;
+
+// Objects
+const sphere: THREE.Mesh = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+  })
+);
+scene.add(sphere);
+
+// Light
+const light: THREE.AmbientLight = new THREE.AmbientLight(
+  0xffffff,
+  1
+);
+scene.add(light);
+
+// Main loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
