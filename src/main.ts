2df005cd55a8aea64b3818ec42ab1972e8d66075
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-10-16 17:15:06 +0300

    refactor

--- src/main.ts
+++ src/main.ts
@@ -112,20 +112,106 @@ class Boid extends Mesh {
 }
 
 
+class Simualtion {
 
-function main () {
+  #boids: Group;
+  #lines: Group;
 
-  //#region  General Configurations
-  const configs = {
+  #configs = {
     boidVelocity: 0.5,
     boidsNumber: 100,
     planeSize: 200,
     lightIntensity: 1,
     boidSize: 1,
-  };
+  }
 
-  const balls = new Group();
-  //#endregion
+  constructor() {
+
+    this.#boids = new Group();
+    this.#lines = new Group();
+
+    this.#create_boids();
+  }
+
+  get boids () {
+    return this.#boids;
+  }
+
+  get lines () {
+    return this.#lines;
+  }
+
+  create_plane () {
+    const plane = new Mesh(
+      new PlaneGeometry( this.#configs.planeSize, this.#configs.planeSize ),
+      new MeshBasicMaterial( {
+        color: 0x000000,
+        side: DoubleSide
+      } )
+    );
+    plane.position.set( 0, 0, 0 );
+    return plane;
+  }
+
+  animateBalls () {
+
+    this.#lines.remove( ...this.#lines.children );
+
+    if ( this.boids.children.length == 0 ) return;
+
+    this.boids.children.forEach( ( boid ) => {
+      this.#lines.add(
+        LineDirection.create( [
+          boid.position,
+          boid.position.clone().add( boid.userData.velocity.clone().multiplyScalar( 20 ) )
+        ] )
+      );
+      boid.position.add( boid.userData.velocity );
+    } );
+
+    const negEdge = -1 * ( this.#configs.planeSize / 2 );
+    const posEdge = this.#configs.planeSize / 2;
+
+    const offset = 2;
+
+
+    this.boids.children.forEach( ( boid ) => {
+      // x col
+      if ( boid.position.x < negEdge ) {
+        boid.position.x += offset;
+        boid.userData.velocity.x *= -1;
+      } else if ( boid.position.x > posEdge ) {
+        boid.position.x -= offset;
+        boid.userData.velocity.x *= -1;
+      }
+
+      // y col
+      if ( boid.position.y < negEdge ) {
+        boid.position.y += offset;
+        boid.userData.velocity.y *= -1;
+      } else if ( boid.position.y > posEdge ) {
+        boid.position.y -= offset;
+        boid.userData.velocity.y *= -1;
+      }
+    } );
+  }
+
+  #create_boids () {
+
+    for ( let i = 0; i < this.#configs.boidsNumber; ++i ) {
+      const boid = new Boid(
+        new SphereGeometry( this.#configs.boidSize, 30, 30 ),
+        new MeshStandardMaterial( {
+          color: Math.random() * 0xffffff,
+        } ),
+        this.#configs.boidVelocity,
+      );
+      this.#boids.add( boid );
+    }
+  }
+}
+
+function main () {
 
   //#region INIT
   // Create Scene
@@ -146,92 +232,33 @@ function main () {
   const light = new LightSetup(
     scene,
     0xffffff,
-    configs.lightIntensity
+    1
   );
+  scene.add( light );
   //#endregion
+
 
   //#region PlayGround
 
-  // Test Objects
-  const plane = new Mesh(
-    new PlaneGeometry( configs.planeSize, configs.planeSize ),
-    new MeshBasicMaterial( {
-      color: 0x000000,
-      side: DoubleSide
-    } )
-  );
-  plane.position.set( 0, 0, 0 );
-  scene.add( plane );
+  // Simualtion
+  const simulation = new Simualtion();
 
+  scene.add( simulation.boids );
+  scene.add( simulation.create_plane() );
+  //scene.add( simulation.lines );
 
-  for ( let i = 0; i < configs.boidsNumber; ++i ) {
-    const boid = new Boid(
-      new SphereGeometry( configs.boidSize, 30, 30 ),
-      new MeshStandardMaterial( {
-        color: Math.random() * 0xffffff,
-      } ),
-      configs.boidVelocity,
-    );
-    balls.add( boid );
-  }
-
-  scene.add( balls );
-
-  const lines = new Group();
-  //scene.add( lines );
-
-  const animateBalls = () => {
-
-    lines.remove( ...lines.children );
-
-    if ( balls.children.length == 0 ) return;
-
-    balls.children.forEach( ( boid ) => {
-      lines.add(
-        LineDirection.create( [
-          boid.position,
-          boid.position.clone().add( boid.userData.velocity.clone().multiplyScalar(10) )
-        ] )
-      );
-      boid.position.add( boid.userData.velocity);
-    } );
-
-    const negEdge = -1 * ( configs.planeSize / 2 );
-    const posEdge = configs.planeSize / 2;
-
-    const offset = 2;
-
-
-    balls.children.forEach( ( boid ) => {
-      // x col
-      if ( boid.position.x < negEdge ) {
-        boid.position.x += offset;
-        boid.userData.velocity.x *= -1;
-      } else if ( boid.position.x > posEdge ) {
-        boid.position.x -= offset;
-        boid.userData.velocity.x *= -1;
-      }
-
-      // y col
-      if ( boid.position.y < negEdge ) {
-        boid.position.y += offset;
-        boid.userData.velocity.y *= -1;
-      } else if ( boid.position.y > posEdge ) {
-        boid.position.y -= offset;
-        boid.userData.velocity.y *= -1;
-      }
-    } );
-  }
   //#endregion
+
 
   //#region GUI
-  const gui = new dat.GUI( { width: 200 } );
-
-  gui.add( configs, "planeSize", 100, 500, 50 ).onChange( () => plane.scale.set( configs.planeSize, configs.planeSize, 0 ) );
-  gui.add( configs, "lightIntensity", 0.1, 1, 0.1 ).onChange( () => light.intensity = configs.lightIntensity );
+  // const gui = new dat.GUI( { width: 200 } );
+  // gui.add( configs, "planeSize", 100, 500, 50 ).onChange( () => plane.scale.set( configs.planeSize, configs.planeSize, 0 ) );
+  // gui.add( configs, "lightIntensity", 0.1, 1, 0.1 ).onChange( () => light.intensity = configs.lightIntensity );
   //#endregion
 
+
   //#region Main loop and events
+
   // On window resize
   const resize = () => {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -243,12 +270,13 @@ function main () {
   // Animation loop
   const animate = () => {
 
-    animateBalls();
+    simulation.animateBalls();
 
     renderer.render( scene, camera );
     requestAnimationFrame( animate );
   }
   animate();
+
   //#endregion
 }
 
